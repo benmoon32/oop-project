fb1c0f9f3bcb96541ba358a733a91c66b99d36ca
Ben <benjamin.moon@edu.sait.ca>
2025-12-15 16:38:59 -0700

    Make program more accurate to output

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# Car class
 class Car:
     def __init__(self, id, name, make, body, year, value):
         self.id = id
@@ -12,16 +11,15 @@ class Car:
         return f"Car(id={self.id}, name={self.name}, make={self.make}, body={self.body}, year={self.year}, value={self.value})"
 
     def __str__(self):
-        return f"{self.id}: {self.name}, {self.make}, {self.body}, {self.year}, ${self.value}"
+        return f"{self.id}    {self.name}    {self.make}    {self.body}    {self.year}    {self.value:.1f}"
 
     def saveString(self):
         return f"{self.id},{self.name},{self.make},{self.body},{self.year},{self.value}"
 
-# Function to load data from file
 def load_data(file):
     cars = []
     try:
-        with open(file, 'r') as file:
+        with open(file, 'w+') as file:
             lines = file.readlines()
             for line in lines:
                 data = line.strip().split(",")
@@ -30,7 +28,6 @@ def load_data(file):
         print(e)
     return cars
 
-# Function to save data to file
 def save_data(file):
     file_contents = ""
     for car in cars:
@@ -47,22 +44,25 @@ def printcars():
         print(car)
 
 def addcar():
-    print("Enter ID of the car, followed by the car's information")
-    car_id = input("ID: ")
-    car_name = input("Name: ")
-    car_make = input("Make: ")
-    car_body = input("Body: ")
-    car_year = input("Year: ")
-    car_value = input("Value: ")
+    print("Enter id of the car, followed by the car's information.")
+    car_id = input("Id: \n")
+    car_name = input("name: \n")
+    car_make = input("make: \n")
+    car_body = input("Body: \n")
+    car_year = input("year: \n")
+    car_value = input("value: \n")
     error = False
     for car in cars:
-        if car.id == car_id:
-            print("Incorrect ID - ID already exists in the system")
+        if car.name == car_name and car.make == car_make and car.body == car_body and car.year == car_year and car.value == car_value:
+            print("The car is already in the inventory. No action is required..")
+            error = True
+        elif car.id == car_id:
+            print("Incorrect Id. Id already exist in the system.")
             error = True
     if error == False:
         new_car = Car(car_id, car_name, car_make, car_body, car_year, car_value)
         cars.append(new_car)
-        print("Car is added to the inventory.")
+        print("car is added to the inventory.")
         print(new_car)
     print("Do you want to add more cars? y(yes)/n(no)")
     add_more = input("> ")
@@ -70,8 +70,28 @@ def addcar():
         addcar()
 
 def editcar():
-    print("Not implemented yet")
+    car_id = ""
+    while car_id != "-1":
+        print("Enter the id of the car. Enter -1 to return to the previous menu")
+        car_id = input("ID: ")
+        car_found = False
+        for car in cars:
+            if car.id == car_id:
+                car_found = True
+                car_name = input("Name: ")
+                car_make = input("Make: ")
+                car_body = input("Body: ")
+                car_year = input("Year: ")
+                car_value = input("Value: ")
 
+                car.name, car.make, car.body, car.year, car.value = car_name, car_make, car_body, car_year, car_value
+
+                print(f"Car's new info is {car}")
+        if not car_found:
+            print("Car not found")
+            
+
+    
 def removecar():
     print("Enter ID of the car that you want to remove from the inventory")
     car_id = input("ID: ")
@@ -86,15 +106,15 @@ def removecar():
 def search():
     search_type = ""
     while search_type != "-1":
-        print("To search using the ID enter 1. To search using the name of the car enter 2. Enter -1 to return to the previous menu")
-        search_type = input("> ")
+        print("To search using the Id enter 1. To search using the name of the car enter 2. Enter -1 to return to the previous menu")
+        search_type = input("")
         if search_type == "1":
-            print("Please enter the ID of the car:")
+            print("Please Enter the id of the car:")
             car_id = input("ID: ")
             car_found = False
             for car in cars:
                 if car.id == car_id:
-                    print(f"Car found: {car}")
+                    print(f"Car found {car}")
                     car_found = True
             if not car_found:
                 print("Car not found")
@@ -104,7 +124,7 @@ def search():
             car_found = False
             for car in cars:
                 if car.name.lower().strip() == car_name.lower().strip():
-                    print(f"Car found: {car}")
+                    print(f"Car found {car}")
                     car_found = True
             if not car_found:
                 print("Car not found")
@@ -120,8 +140,8 @@ print("Welcome to the cars inventory system")
 command = ""
 while command != "0":
     print("What would you like to do today?")
-    print("Add a car? Enter 1\nSearch for car? Enter 2\nEdit car info? Enter 3\nRemove a car? Enter 4\nPrint the car list? Enter 5\nSave the data to a file? Enter 6\nExit? Enter 0")
-    command = input("> ")
+    print("Add a car? enter 1\nSearch for car? enter 2\nEdit car info? enter 3\nRemove a car? enter 4\nPrint the car list? enter 5\nSave the data to a file? enter 6\nExit? enter 0.")
+    command = input("")
     if command == "1":
         addcar()
     elif command == "2":
